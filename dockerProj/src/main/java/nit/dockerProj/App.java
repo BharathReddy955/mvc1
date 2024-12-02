8490f1dbb78def596f59ecc86aada74730c4e57e
Bharath Reddy <bsrassignment@gmail.com>
2024-12-02 17:14:25 +0530

    t

--- dockerProj/src/main/java/nit/dockerProj/App.java
+++ dockerProj/src/main/java/nit/dockerProj/App.java
@@ -3,5 +3,6 @@ package nit.dockerProj;
 public class App {
     public static void main(String[] args) {
         System.out.println("welcome to docker");
+        System.out.println("App.main()");
     }
 }
